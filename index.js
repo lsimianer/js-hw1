401750da9dba7d7745dac80b55fd91ef110c4d50
lukas simianer <lsimianer@gmail.com>
2019-05-29 07:22:21 -0500

    restarted, new plan of attack

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-js
+var wordbank = 
+["tibia",
+"ankle",
+"femur",
+"cocyx",
+"pelvis",
+"patella",
+"hip",
+"cranium",
+];
+
+const maxTries = 10;
+
+var guessedLetters = [];
+var currentWordIndex = [];
+var guessingWord = [];
+var remainingGuesses = 0;
+var gameStarted = false;
+var hasFinished = false;
+var wins = 0;
+
+// Game logic, word bank array, and global data needed listed above ^^
+
+
+
+
+        
